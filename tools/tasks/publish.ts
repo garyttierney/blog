7cb73d78f8b7b0a51f1b9330c8b228a716dd8222
Gary Tierney <gary.tierney@gmx.com>
2016-12-18 21:07:10 +0000

    Use gulp publish task instead of shell script

---
+++ tools/tasks/publish.ts
@@ -0,0 +1,16 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import {config} from '../config/project';
+import {join} from 'path';
+import * as runSequence from 'run-sequence';
+
+let plugins = <any>gulpLoadPlugins();
+
+export = function (cb : any) {
+    return gulp.src(join(config.BUILD_DEST, '**', '*'))
+        .pipe(plugins.ghPages({
+            force: true,
+            branch: 'master',
+            origin: 'sites'
+        });
+};
